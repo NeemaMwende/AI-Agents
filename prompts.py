4b02fa9848cd74a94bde1f84d3a46837c6cdd294
Neema Mwende <gracepete12@gmail.com>
2025-11-13 15:58:34 +0300

    prompt engineering

--- prompts.py
+++ prompts.py
@@ -49,4 +49,57 @@ print(f"response : {response}\n")
 
 
 
+# few shot prompt
+params = {
+"max_new_tokens": 10,
+}
+prompt = """Here are few examples of classifying emotions in statements:
+Statement: 'I just won my first marathon!'
+Emotion: Joy
+Statement: 'I can't believe I lost my keys again.'
+Emotion: Frustration
+Statement: 'My best friend is moving to another country.'
+Emotion: Sadness
+Now, classify the emotion in the following statement:
+Statement: 'That movie was so scary I had to cover my eyes.’
+"""
+response = llm_model(prompt, params)
+print(f"prompt: {prompt}\n")
+print(f"response : {response}\n")
+
+
+
+
+# chain of thought prompt 
+params = {
+"max_new_tokens": 512,
+"temperature": 0.5,
+}
+prompt = """Consider the problem: 'A store had 22 apples. They sold 15 apples␣
+↪today and got a new delivery of 8 apples.
+How many apples are there now?’
+Break down each step of your calculation
+"""
+response = llm_model(prompt, params)
+print(f"prompt: {prompt}\n")
+print(f"response : {response}\n")  
+
+
+
+
+
+# Self-consistency 
+params = {
+"max_new_tokens": 512,
+}
+prompt = """When I was 6, my sister was half of my age. Now I am 70, what age␣
+↪is my sister?
+Provide three independent calculations and explanations, then␣
+determine the most consistent result.
+↪
+"""
+response = llm_model(prompt, params)
+print(f"prompt: {prompt}\n")
+print(f"response : {response}\n")
+
 
